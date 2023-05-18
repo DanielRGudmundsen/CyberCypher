fda1e67e5b2bdb2a5fbed46b745255a59ed99eb4
Daniel Gudmundsen <danielrgudmundsen@outlook.com>
2023-05-17 18:59:50 -0700

    Created a README and uploaded a screenshot to be used in it

---
+++ README.md
@@ -0,0 +1,36 @@
+# Password Generator
+
+This repository contains a simple and interactive password generator web application. It helps users create strong, random passwords according to customizable options. 
+
+![Password Generator Screenshot](./password-generator-screenshot.png)
+
+## Features
+
+1. **Customizable password length:** The user can specify the length of the password, choosing a value between 8 and 128 characters.
+
+2. **Customizable character types:** The user can choose to include any combination of the following character types in their password:
+   - Lowercase letters
+   - Uppercase letters
+   - Numbers
+   - Special characters (e.g., `!@#$%^&*()-_=+{}[]|:;,<>.?/`)
+
+3. **Interactive user interface:** The application prompts the user to select their password preferences, and it displays the generated password in a text area on the webpage.
+
+## Usage
+
+To use this password generator, simply load the webpage, click the "Generate Password" button, and follow the prompts to customize your password.
+
+After the password is generated, it will appear in the text area. You can then copy the password and use it wherever you need a strong, random password.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- jQuery
+
+## Possible Future Updates
+
+- **jQuery dialog boxes:** Improve the user interface by switching from JavaScript prompts and confirm dialog boxes to more user-friendly and aesthetically pleasing jQuery dialog boxes.
+- **Password strength estimator:** An algorithm to estimate and display the strength of the generated password, based on length and character types used.
+- **Copy Password Button** A button that saves the generated password to your clipboard.
--- script.js
+++ script.js
@@ -1,19 +1,25 @@
+// Ensure the code does not run until the document is fully loaded
 $(document).ready(function() {
-// to-do stop the function if the user presses cancel on the prompt
+
+// Main password generation function
   function generatePassword() {
     var length;
+
+    // This while loop will continuously prompt the user to enter a password length until they enter a valid length (between 8 and 128 characters)
     while (true) {
       length = parseInt(prompt("Please enter the desired password length. Must be between 8 and 128 characters."));
       if (length >= 8 && length <= 128) break;
       alert("Invalid input. Length must be between 8 and 128 characters.");
-      
+      // to-do stop the function if the user presses cancel on the prompt
     }
 
+    // These variables will store the user's preferences for what types of characters to include in the password
     var useLowercase = confirm("Do you want to include lowercase characters?");
     var useUppercase = confirm("Do you want to include uppercase characters?");
     var useNumbers = confirm("Do you want to include numeric characters?");
     var useSpecial = confirm("Do you want to include special characters?");
 
+    // If the user doesn't select any character type, it will show an alert and restart the function
     if (!useLowercase && !useUppercase && !useNumbers && !useSpecial) {
       alert("You must select at least one character type.");
       return generatePassword(); // restart the function if no type selected
@@ -22,17 +28,20 @@ $(document).ready(function() {
     var password = "";
     var possible = "";
 
+    // Depending on the user's preferences, different types of characters are added to the pool of possible characters
     if (useLowercase) possible += "abcdefghijklmnopqrstuvwxyz";
     if (useUppercase) possible += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     if (useNumbers) possible += "0123456789";
     if (useSpecial) possible += "!@#$%^&*()-_=+{}[]|:;,<>.?/";
 
+    // This loop generates the password, character by character, selecting randomly from the pool of possible characters
     for (var i = 0; i < length; i++)
       password += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return password;
   }
 
+  // When the "Generate Password" button is clicked, the password is generated and then displayed in the #password textarea
   $("#generate").on("click",()=>{
     var password = generatePassword();
     var passwordText = $("#password");
